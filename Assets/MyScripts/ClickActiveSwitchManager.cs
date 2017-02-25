b604919820a8ef58acc4dcb313885110b9a5eed2
tct <tct@nothing.local>
2017-02-26 01:34:15 +0900

    クリックによってコメントを表示・非表示する処理を追加

---
+++ Assets/MyScripts/ClickActiveSwitchManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickActiveSwitchManager : MonoBehaviour {
+
+	public GameObject targetObj;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("mouse 0")) {
+			if (targetObj.activeSelf) {
+				targetObj.SetActive (false);
+			} else {
+				targetObj.SetActive (true);
+			}
+		}
+	}
+}
---
+++ Assets/MyScripts/ClickCreateObjectManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickCreateObjectManager : MonoBehaviour {
+
+	public GameObject objSphere;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("mouse 0")) {
+			GameObject.Instantiate (objSphere, new Vector3 (0, 0, 10), new Quaternion ());
+		}
+	}
+}
