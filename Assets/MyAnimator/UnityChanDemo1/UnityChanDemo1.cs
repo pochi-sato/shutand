2e0db2de40b7a3816c479c34fdf292b03ae0246c
tct <tct@nothing.local>
2017-02-27 22:33:30 +0900

    クリック時に、unityちゃんがジャンプ・歩く、のアクションをできるようにした

---
+++ Assets/MyAnimator/UnityChanDemo1/UnityChanDemo1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanDemo1 : MonoBehaviour {
+
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("mouse 0")) {
+			if (animator.GetBool("is_running")) {
+				animator.SetBool("is_running", false);
+			} else {
+				transform.position += transform.forward * 0.01f;
+				animator.SetBool("is_running", true);
+			}
+		}
+		if (Input.GetKey("right")) {
+			transform.Rotate(0, 10, 0);
+		}
+		if (Input.GetKey ("left")) {
+			transform.Rotate(0, -10, 0);
+		}
+	}
+}
