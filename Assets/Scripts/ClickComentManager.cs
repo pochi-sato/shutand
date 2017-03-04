edf4079f5cff2d5275df79d180a75c01f363b0a4
tct <tct@nothing.local>
2017-03-04 15:32:19 +0900

    うん、でコメントが進むようにした

--- Assets/Scripts/ClickComentManager.cs
+++ Assets/Scripts/ClickComentManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using UnityEngine.Networking;
 
@@ -11,6 +12,9 @@ public class ClickComentManager : MonoBehaviour {
 	GameObject[] thirds;
 	GameObject[] fourths;
 
+	// unun
+	DateTime sendTime;
+
 	void Change(GameObject[] objs, bool nextStatus) {
 		foreach (GameObject obj in objs) {
 			obj.SetActive (nextStatus);
@@ -19,6 +23,10 @@ public class ClickComentManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// unun
+		sendTime = DateTime.Now;
+
+
 		firsts = GameObject.FindGameObjectsWithTag("1stAction");
 		seconds = GameObject.FindGameObjectsWithTag("2ndAction");
 		thirds = GameObject.FindGameObjectsWithTag("3rdAction");
@@ -39,46 +47,109 @@ public class ClickComentManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("mouse 0")) {
-			switch (count) {
-			case 0:
-				Debug.Log ("----------------------------1");
-				Change (firsts, true);
-				break;
-			case 1:
-				Debug.Log ("----------------------------2");
-				Change (firsts, false);
-				Change (seconds, true);
-				break;
-			case 2:
-				Debug.Log ("----------------------------3");
-				Change (seconds, false);
-				Change (thirds, true);
-				break;
-			case 3:
-				Debug.Log ("----------------------------4");
-				Change (thirds, false);
-				Change (fourths, true);
-				StartCoroutine(postMessage());
-				break;
-			case 4:
-				Debug.Log ("----------------------------5");
-				Change (fourths, false);
-				count = 0;
-				break;
-			default:
-				Debug.Log ("----------------------------df");
-				Change (firsts, false);
-				Change (seconds, false);
-				Change (thirds, false);
-				Change (fourths, false);
 
-				break;
+		if(sendTime.AddSeconds(1.5).CompareTo(DateTime.Now) < 0){
+			string s = $"{Input.gyro.rotationRate.x},{Input.gyro.rotationRate.y},{Input.gyro.rotationRate.z}";
+			Debug.Log(s);
+			if (Input.gyro.rotationRate.x > 2.9) {
+				Debug.Log ("00000000000000000000000000000000000000000000000000 YEEEEESSS!!!");
+
+
+				// ここからununの内容処理
+
+				switch (count) {
+				case 0:
+					Debug.Log ("----------------------------1");
+					Change (firsts, true);
+					break;
+				case 1:
+					Debug.Log ("----------------------------2");
+					Change (firsts, false);
+					Change (seconds, true);
+					break;
+				case 2:
+					Debug.Log ("----------------------------3");
+					Change (seconds, false);
+					Change (thirds, true);
+					break;
+				case 3:
+					Debug.Log ("----------------------------4");
+					Change (thirds, false);
+					Change (fourths, true);
+					StartCoroutine(postMessage());
+					break;
+				case 4:
+					Debug.Log ("----------------------------5");
+					Change (fourths, false);
+					count = 0;
+					break;
+				default:
+					Debug.Log ("----------------------------df");
+					Change (firsts, false);
+					Change (seconds, false);
+					Change (thirds, false);
+					Change (fourths, false);
+
+					break;
+				}
+				count = count + 1;
+
+				// ここまでununの内容処理
+
+
+				// 後処理
+				sendTime = DateTime.Now;
+
+			} else if (Input.gyro.rotationRate.y > 2.9) {
+				Debug.Log ("いやだお");
 			}
 
-			count = count + 1;
-
 		}
+
+
+
+
+
+//		if (Input.GetKey ("mouse 0")) {
+//			switch (count) {
+//			case 0:
+//				Debug.Log ("----------------------------1");
+//				Change (firsts, true);
+//				break;
+//			case 1:
+//				Debug.Log ("----------------------------2");
+//				Change (firsts, false);
+//				Change (seconds, true);
+//				break;
+//			case 2:
+//				Debug.Log ("----------------------------3");
+//				Change (seconds, false);
+//				Change (thirds, true);
+//				break;
+//			case 3:
+//				Debug.Log ("----------------------------4");
+//				Change (thirds, false);
+//				Change (fourths, true);
+//				StartCoroutine(postMessage());
+//				break;
+//			case 4:
+//				Debug.Log ("----------------------------5");
+//				Change (fourths, false);
+//				count = 0;
+//				break;
+//			default:
+//				Debug.Log ("----------------------------df");
+//				Change (firsts, false);
+//				Change (seconds, false);
+//				Change (thirds, false);
+//				Change (fourths, false);
+//
+//				break;
+//			}
+//
+//			count = count + 1;
+//
+//		}
 	}
 
 	IEnumerator postMessage(){
