d2e51c9ee650cf873f5ff3aea93c8a1b23c9e59b
tct <tct@nothing.local>
2017-03-04 14:39:41 +0900

    コメント、しゅたんどを常にカメラを見るように変更

---
+++ Assets/Scripts/DirectionHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionHolder : MonoBehaviour {
+
+	public Camera targetCamera;
+
+	// Use this for initialization
+	void Start () {
+		//対象のカメラが指定されない場合にはMainCameraを対象とします。
+		if (this.targetCamera == null)
+			targetCamera = Camera.main;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//カメラの方向を向くようにする。
+		this.transform.LookAt(this.targetCamera.transform.position);    
+	}
+}
